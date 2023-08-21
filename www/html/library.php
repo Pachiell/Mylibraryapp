79db884b8c830cf608c3f62b869c12b2a5182d47
Pachiell <gundamms310@yahoo.co.jp>
2023-08-22 04:56:20 +0900

    ライブラリとデータベースの連結　完了

--- www/html/library.php
+++ www/html/library.php
@@ -3,9 +3,13 @@
 include '../model/connect.php';
 include '../model/user.php';
 include '../model/secure.php';
+include '../model/archive.php';
 
 
 $userName = $_SESSION["name"];
+$userId = $_SESSION["user_id"];
 
+$archive= new Archive();
+$archives=$archive->find($userId);
 
 include_once "../view/library_view.php";
